0832ca3a2ae66aaa3cb8d65d6aa559fa909d89ec
unknown <ali.speed6@gmail.com>
2014-07-15 12:08:23 -0700

    alastair first commit

---
+++ CSharpTestProject1/CSharpTestProject1/Products.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTestProject1
+{
+    class Products
+    {
+        private string prodid;
+        private string prodname;
+        private int stock;
+        private int unitprice;
+
+        public Products()
+        {
+            prodid = "";
+            prodname = "";
+            stock = 0;
+            unitprice = 0;
+        }
+        public Products(string id, string name, int stk, int prc)
+        {
+            prodid = id;
+            prodname = name;
+            stock = stk;
+            unitprice = prc;
+
+        }
+        public string Prodid
+        {
+            get { return prodid; }
+            set { prodid = value; }
+        }
+        public string Prodname
+        {
+            get { return prodname; }
+            set { prodname = value; }
+        }
+        public int Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+        public int Unitprice
+        {
+            get { return unitprice; }
+            set { unitprice = value; }
+        }
+    }
+}
